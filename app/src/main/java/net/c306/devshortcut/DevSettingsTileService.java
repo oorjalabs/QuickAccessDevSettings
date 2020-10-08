8dba5709b0fff139dd960f7d6b44a5c7b48466b4
Aditya <adityabhaskar@users.noreply.github.com>
2020-10-08 08:14:20 +0100

    Add empty tile service to manifest

---
+++ app/src/main/java/net/c306/devshortcut/DevSettingsTileService.java
@@ -0,0 +1,8 @@
+package net.c306.devshortcut;
+
+import android.service.quicksettings.TileService;
+
+public class DevSettingsTileService extends TileService {
+    // TODO: 07/10/2020 Create a tile to add to quick settings
+
+}
